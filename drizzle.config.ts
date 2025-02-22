fb11fad69a1978fd2b1ea45f252034583c73e979
Mohamed Mayla <4347943+mmayla@users.noreply.github.com>
2025-02-22 15:27:07 +0200

    feat: Pick the correct drizzle config based on env vars

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,26 @@
 import './envConfig.ts'
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig, type Config } from 'drizzle-kit';
 
-export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'turso',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN
-  },
-});
+let drizzleConfig: Config;
+if (process.env.DATABASE_AUTH_TOKEN) {
+  drizzleConfig = {
+    out: './drizzle',
+    schema: './src/db/schema.ts',
+    dialect: 'turso',
+    dbCredentials: {
+      url: process.env.DATABASE_URL!,
+      authToken: process.env.DATABASE_AUTH_TOKEN
+    },
+  }
+} else {
+  drizzleConfig = {
+    out: './drizzle',
+    schema: './src/db/schema.ts',
+    dialect: 'sqlite',
+    dbCredentials: {
+      url: process.env.DATABASE_URL!
+    },
+  }
+}
+
+export default defineConfig(drizzleConfig);
